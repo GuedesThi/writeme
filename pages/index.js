424664b3f1d15d6945752789eeedd888522d41dc
GuedesThi <thiagodesignguedes@gmail.com>
2024-08-09 17:14:02 -0300

    add: propósito do site descrito em `index.js`, e, algumas modificações no arquivo `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,13 @@ function Home() {
   return (
     <div>
       <h1>writeme</h1>
+      <p>
+        Um local para pessoas que <strong>querem</strong> pegar histórias que
+        são contadas e escritas apenas em suas cabeças
+      </p>
+      <p>e, trazê-las para o mundo real (ou pelo menos virtual)</p>
     </div>
   );
-}
-
-function test() {
-  console.log("oi");
 }
 
 export default Home;
