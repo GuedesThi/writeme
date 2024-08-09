1fb797228c2759959228defef2d78c2207110a06
GuedesThi <thiagodesignguedes@gmail.com>
2024-08-09 00:00:10 -0300

    add: `lint:check` and `lint:fix` files for code formatation

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # writeme
+
 Application for writers of books, novels, short stories, etc. to have a place to create their stories and help other people with their projects.
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 function Home() {
-    return (
-        <div>
-            <h1>writeme</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>writeme</h1>
+    </div>
+  );
+}
+
+function test() {
+  console.log("oi");
 }
 
 export default Home;
