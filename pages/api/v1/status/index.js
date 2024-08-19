2740ec6e188474613d179472fd5bc8a9c5f717d0
GuedesThi <thiagodesignguedes@gmail.com>
2024-08-18 21:16:22 -0300

    finalização da `API`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,24 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result);
-  response.status(200).json("writeme vai bombar");
+  const updatedAt = new Date().toISOString();
+
+  const versionRes = await database.query("SHOW server_version;");
+  const maxRes = await database.query("SHOW max_connections;");
+  const openRes = await database.query(
+    "SELECT count(*) FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: parseInt(maxRes.rows[0].max_connections),
+        opened_connections: parseInt(openRes.rows[0].count),
+        version: versionRes.rows[0].server_version,
+      },
+    },
+  });
 }
 
 export default status;
