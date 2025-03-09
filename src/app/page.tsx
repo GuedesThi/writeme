fc6aef0b81f807ad2a05f2ef9730fbb0884b2271
GuedesThi <thiagodesignguedes@gmail.com>
2025-03-09 14:39:34 -0300

    feat: finalizando layout da página principal, e adicionando responsividade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import Card from "@/components/Card";
 import styles from "./page.module.css";
 import { EB_Garamond, Poppins } from "next/font/google";
 
-// className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+// className={`${poppins.className} font-normal`}
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400"],
@@ -10,7 +11,7 @@ const poppins = Poppins({
 });
 
 const ebGaramond = EB_Garamond({
-  weight: ["400", "500", "600", "700", "800"], // Defina os pesos que deseja usar
+  weight: ["400", "500", "600", "700", "800"],
   subsets: ["latin"],
   style: ["normal", "italic"],
 });
@@ -19,24 +20,42 @@ export default function Home() {
   return (
     <div className="p-[60px]">
       {/* Cabeçalho */}
-      <div>
-        <h1 className={`${poppins.className} font-light italic text-[27px]`}>
+      <div className="max-md:flex md:justify-normal max-md:justify-center max-md:items-center">
+        <h1
+          className={`${poppins.className} font-light italic md:text-[27px] max-md:text-[17px]`}
+        >
           writeme.com.br
         </h1>
       </div>
 
       {/* Frase inicial */}
-      <div className="flex flex-col justify-center items-center pt-24">
-        <p className={`${ebGaramond.className} text-[27px]`}>
+      <div className="flex flex-col justify-center items-center pt-20">
+        <p
+          className={`${ebGaramond.className} md:text-[27px] max-md:text-[20px]`}
+        >
           "Do mundo pra mente, da mente pras páginas, das páginas pro mundo",
         </p>
-        <p className={`${ebGaramond.className} font-light text-[27px]`}>
+        <p
+          className={`${ebGaramond.className} font-light md:text-[27px] max-md:text-[20px]`}
+        >
           oguedes
         </p>
       </div>
 
       {/* Opções inicial */}
-      <div className="flex items-center"></div>
+      <div className="flex md:flex-row max-md:flex-col items-center justify-center gap-12 pt-16 px-5">
+        <Card
+          pathUrl="/create-book"
+          imageUrl="/icon-plus.png"
+          content="Escrever algo"
+        />
+        <Card pathUrl="/lib" imageUrl="/icon-book.png" content="Minhas obras" />
+        <Card
+          pathUrl="/account"
+          imageUrl="/icon-profile.png"
+          content="Acessar sua conta"
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/Card/index.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import Link from "next/link";
+
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400"],
+  subsets: ["latin"],
+  style: ["normal", "italic"],
+});
+
+interface CardParams {
+  pathUrl: string;
+  imageUrl: string;
+  content: string;
+}
+
+export default function Card({ pathUrl, imageUrl, content }: CardParams) {
+  return (
+    <>
+      <Link
+        href={pathUrl}
+        className="group hover:bg-slate-300 hover:border-white flex flex-1 flex-col max-md:h-[200px] max-md:w-[200px] text-center justify-center items-center gap-10 px-4 py-8 cursor-pointer border border-gray-600 rounded-lg"
+      >
+        <Image
+          src={imageUrl}
+          width={65}
+          height={70}
+          alt={content}
+          className="cursor-pointer"
+        />
+        <p
+          className={`${poppins.className} group-hover:text-gray-800 w-full md:text-[16px] max-md:text-[13px] text-gray-600 cursor-pointer`}
+        >
+          {content}
+        </p>
+      </Link>
+    </>
+  );
+}
